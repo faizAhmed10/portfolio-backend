01052a512b7dc814a2909b503a2dd99094882aa3
faizAhmed10 <faizahmed10604@gmail.com>
2024-08-01 22:23:41 +0530

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG=True
+DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG=True
 
 
 ALLOWED_HOSTS = ['*']

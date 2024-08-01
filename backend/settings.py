7ee3c521a134e05e19bdc3823fa5fd48be3ca97e
faizAhmed10 <faizahmed10604@gmail.com>
2024-08-01 21:57:18 +0530

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
